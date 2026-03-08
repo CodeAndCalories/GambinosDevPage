0d2632d58c0fb0f21562c12e85ffd743287024a7
Gambino <axigamingclips@gmail.com>
2026-03-08 17:36:51 -0400

    Fix metadata build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Gambino Labs",
-  template: "%s | Gambino Labs",
   description: "Experimental software • tools • creative systems",
 };
 
